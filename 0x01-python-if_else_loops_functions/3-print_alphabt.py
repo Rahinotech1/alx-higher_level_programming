11e9ba3b7e8d0bc6405dcb7f21bca627a2b66e2e
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-01 09:48:20 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord('a'), ord('z') + 1):
     if chr(a) != 'e' and chr(a) != 'q':
-        print("{:a, z}".format(a), end='')
+        print("{:a}".format(a), end='')
