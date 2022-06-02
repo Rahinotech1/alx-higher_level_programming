d9c0e6412f491632c8a9ec4d4cbad9a9f557ba14
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-02 09:22:36 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    count = len(names)
-    for i in range(8, count):
-        print("{:s}".format(names[i]))
+    for i in range(len(names)):
+        if names[i][:2] != '__':
+            print('{:s}'.format(names[i]))
