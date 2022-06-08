6cd56bc392ecd6adb2f78f66987803e2470c45c4
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-08 14:49:53 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = []
-    for a in range(len(my_list)):
-        if my_list[a] != search:
-            copy.append(my_list[a])
-        else:
-            copy.append(replace)
-            return copy
+    return([replace if number == search else number for number in my_list])
