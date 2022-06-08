58c7e3fdcbe5b5a990a3d96f496b77e4637174ed
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-08 16:12:34 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or len(my_dict) == 0:
-        return None
-    return sorted(my_dict, key=lambda score: score[2])[-1]
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_value = max(a_dictionary, key=a_dictionary.get)
+    else:
+        max_value = None
+        return(max_value)
+
