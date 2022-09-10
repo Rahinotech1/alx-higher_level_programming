0448715de8ef2b799ddd83b6b9d2729d0b2dafd0
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-09-10 16:11:12 +0100

    Filter state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""script for use in getting all states from sql db
+"""Script to list all states with name starting with `N` from
+database `hbtn_0e_0_usa`
 """
-import MySQLdb
-import sys
 
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-if __name__ == '__main__':
-    args = sys.argv
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data,
-                         port=3306)
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
     cur = db.cursor()
-    num_rows = cur.execute('''
-            SELECT * FROM states
-            WHERE states.name LIKE 'N%'
-            ORDER BY states.id
-            ''')
-    rows = cur.fetchall()
-    for row in rows:
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE 'N%' "
+                "COLLATE 'latin1_general_cs' "
+                "ORDER BY id ASC")
+    for row in cur.fetchall():
         print(row)
+    cur.close()
+    db.close()
