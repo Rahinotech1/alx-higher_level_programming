c992b2eeb23c258d751d4c5fa4f48a93f12771e4
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-10-27 09:20:14 -0700

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Script to request content from `https://intranet.hbtn.io/status`"""
+"""Script to request content from `https://alx-intranet.hbtn.io/status`"""
 
 import urllib.request
 import urllib.error
 
 try:
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         body = r.read()
         print('Body response:')
         print('\t- type: {}'.format(type(body)))
